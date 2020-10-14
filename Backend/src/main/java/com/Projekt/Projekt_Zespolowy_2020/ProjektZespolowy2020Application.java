a15fd84699c27ecbe947487ef6d4a7c779bd9c41
Mateusz Soloducha <alamakotaisi@gmail.com>
2020-10-14 14:01:05 +0200

    Backend

---
+++ Backend/src/main/java/com/Projekt/Projekt_Zespolowy_2020/ProjektZespolowy2020Application.java
@@ -0,0 +1,13 @@
+package com.Projekt.Projekt_Zespolowy_2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektZespolowy2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektZespolowy2020Application.class, args);
+	}
+
+}
