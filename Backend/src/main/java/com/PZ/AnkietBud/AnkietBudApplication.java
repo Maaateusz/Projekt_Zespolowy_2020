2025abb40b2da313c51a62c2630c10a47fbab497
Mateusz Soloducha <alamakotaisi@gmail.com>
2020-11-03 14:33:42 +0100

    Szkielet v1

---
+++ Backend/src/main/java/com/PZ/AnkietBud/AnkietBudApplication.java
@@ -0,0 +1,13 @@
+package com.PZ.AnkietBud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkietBudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnkietBudApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/PZ/AnkietBud/AnkietBudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PZ.AnkietBud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnkietBudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
