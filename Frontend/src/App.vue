2f086333a1932f9dad0dc55a8b4978e5862ae84d
pprzygodzk <48103797+pprzygodzk@users.noreply.github.com>
2020-11-10 00:01:40 +0100

    Frontend

---
+++ Frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Logo AnkietBud" src="logo.png">
+    <MakeNewForm />
+  </div>
+</template>
+
+<script>
+import MakeNewForm from './components/MakeNewForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MakeNewForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #0f0f0f;
+  margin-top: 60px;
+}
+</style>
