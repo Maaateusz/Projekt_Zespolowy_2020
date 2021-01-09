458a36de1c2a842bcf76827a81fb33616ed76af9
skrzyszewska <72800233+skrzyszewska@users.noreply.github.com>
2021-01-09 18:32:15 +0100

    add vue-router

---
+++ Frontend/src/views/About.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="about">
+    <h3>TEXTBOX</h3>
+    <input v-model="name" placeholder="Enter Name" />
+    <h3>
+      <p>Name entered is : {{ name }}</p>
+    </h3>
+    <hr />
+    <h3>Textarea</h3>
+    <textarea v-model="textmessage" placeholder="Add Details"></textarea>
+    <h3>
+      <p>Text: {{ textmessage }}</p>
+    </h3>
+    <hr />
+    <h3>Checkbox</h3>
+    <input type="checkbox" id="checkbox" v-model="checked" /> {{ checked }}
+    <hr />
+    <h3>Radio</h3>
+    <input type="radio" id="black" value="Black" v-model="picked" />Black
+    <input type="radio" id="white" value="White" v-model="picked" />White
+    <h3>Radio element clicked : {{ picked }}</h3>
+    <hr />
+    <h3>Select</h3>
+    <select v-model="languages">
+      <option disabled value="">Please select one</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+      <option>D</option>
+    </select>
+    <h3>Languages Selected is : {{ languages }}</h3>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about",
+  props: {
+    name: String,
+    textmessage: String,
+    languages: String,
+    picked() {}
+  }
+};
+</script>
---
+++ Frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Logo" src="../assets/logo.png" />
+    <Hello msg="Welcome" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Hello from "@/components/Hello";
+export default {
+  name: "Home",
+  components: {
+    Hello
+  }
+};
+</script>
