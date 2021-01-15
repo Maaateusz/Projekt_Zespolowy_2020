f9a07dc3a3a86b6de0590036a307cd200a3b790a
skrzyszewska <72800233+skrzyszewska@users.noreply.github.com>
2021-01-15 18:18:22 +0100

    add vue-slider, corretion sum value

--- Frontend/src/components/QuestionScale.vue
+++ Frontend/src/components/QuestionScale.vue
@@ -2,17 +2,16 @@
 <!-- TODO  do poprawek typy inputow, tam gdzie liczby to trzeba ustawic liczby-->
   <div>
     <b-form-input v-model="content" placeholder="Treść pytania..."></b-form-input>
-    <b-form-input v-model="valueMin" placeholder="Wartość minimalna..."></b-form-input>
-    <b-form-input :for="`type-number`" v-model="valueMax" placeholder="Wartość maksymalna..."></b-form-input>
+    <b-form-input :for="`type-number`" v-model.number="valueMin" placeholder="Wartość minimalna..."></b-form-input>
+    <b-form-input :for="`type-number`" v-model.number="valueMax" placeholder="Wartość maksymalna..."></b-form-input>
     {{ (valueMin && valueMax) != null ? valueMin : ""}}
     <b-form-input v-model="anchorLeft" placeholder="Skrajnie lewa wartość..."></b-form-input>
-    {{ (valueMin+valueMax)/2}}
+    {{ (valueMin + valueMax)/2 }}
     <b-form-input v-model="anchorCentral" placeholder="Centralna wartość..."></b-form-input>
     {{valueMax}}
     <b-form-input v-model="anchorRight" placeholder="Skrajnie prawa wartość..."></b-form-input>
     <div>
-      <label for="range-2">{{content}}</label>
-      <b-form-input id="range-2" v-model="tempValue" type="range" :min="valueMin" :max="valueMax" step="1"></b-form-input>
+      <vue-slider v-model="value" type="range" :min="valueMin" :max="valueMax" step="1" />
       <div class="scaleAnchors">
         <p class="scaleAnchor">{{anchorLeft}}</p>
         <p class="scaleAnchor">{{anchorCentral}}</p>
@@ -24,17 +23,24 @@
 </template>
 
 <script>
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/antd.css'
+
+
 export default {
   name: "QuestionScale",
+  components: {
+    VueSlider
+  },
   data(){
     return{
+      value: 0,
       content: '',
       valueMax: 1,
       valueMin: 0,
       anchorLeft: '',
       anchorCentral: '',
-      anchorRight: '',
-      tempValue: 0 //TODO same as in slider
+      anchorRight: ''
     }
   }
 }
@@ -49,3 +55,4 @@ export default {
   flex: 1;
 }
 </style>
+
