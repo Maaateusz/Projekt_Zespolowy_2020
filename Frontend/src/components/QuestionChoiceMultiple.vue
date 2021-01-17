4d82ab85d7cd086183b35e079483a300eb242031
skrzyszewska <72800233+skrzyszewska@users.noreply.github.com>
2021-01-17 13:59:22 +0100

    questionChoice-Design

--- Frontend/src/components/QuestionChoiceMultiple.vue
+++ Frontend/src/components/QuestionChoiceMultiple.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
-    <b-form-input v-model="content" placeholder="Treść pytania..."></b-form-input>
+  <div class="questionChoiceMultiple">
+   <div><b-form-input v-model="content" placeholder="Treść pytania..."></b-form-input></div>
     <b-form-group v-slot="{ ariaDescribedby }">
-      <b-form-checkbox disabled="true" v-for="answer in answers"  :key="answer" :aria-describedby="ariaDescribedby"  name="answers">
-        <input type="text" v-model="answer.content" :key="index"/>
+      <div class="answerValue">
+      <div><b-form-checkbox disabled="true" v-for="answer in answers"  :key="answer" :aria-describedby="ariaDescribedby"  name="answers">
+        <input class="form-control form-rounded" type="text" v-model="answer.content" :key="index"/>
       </b-form-checkbox>
+      </div>
+  </div>
     </b-form-group>
-    <b-button v-on:click="addNewAnswer" pill variant="success">+</b-button>
+  <b-button v-on:click="addNewAnswer" pill variant="success">+</b-button>
   </div>
 </template>
 
@@ -32,5 +35,18 @@ export default {
 </script>
 
 <style scoped>
+
+.questionChoiceMultiple{
+  padding: 0.5em;
+  border: 0.20em outset #ccc;
+  border-radius: 0.15em;
+}
+.answerValue{
+  flex-direction: column;
+  display: flex;
+  float: left;
+  width: auto;
+  padding: 1em;
+}
 
 </style>
--- Frontend/src/components/QuestionChoiceSingle.vue
+++ Frontend/src/components/QuestionChoiceSingle.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-    <b-form-input v-model="content" placeholder="Treść pytania..."></b-form-input>
+  <div class="questionChoiceSingle">
+    <div><b-form-input v-model="content" placeholder="Treść pytania..."></b-form-input></div>
     <b-form-group v-slot="{ ariaDescribedby }">
-      <b-form-radio disabled="true" v-for="answer in answers"  :key="answer" :aria-describedby="ariaDescribedby"  name="answers">
-        <input type="text" v-model="answer.content" :key="index"/>
+      <div class="answerValue">
+      <div><b-form-radio disabled="true" v-for="answer in answers"  :key="answer" :aria-describedby="ariaDescribedby"  name="answers">
+        <input class="form-control form-rounded" type="text" v-model="answer.content" :key="index"/>
       </b-form-radio>
+      </div>
+      </div>
     </b-form-group>
     <b-button v-on:click="addNewAnswer" pill variant="success">+</b-button>
   </div>
@@ -32,5 +35,19 @@ export default {
 </script>
 
 <style scoped>
+.questionChoiceSingle{
+  padding: 0.5em;
+  border: 0.20em outset #ccc;
+  border-radius: 0.15em;
+}
+.answerValue{
+  flex-direction: column;
+  display: flex;
+  float: left;
+  width: auto;
+  padding: 1em;
+}
+button{
 
+}
 </style>
