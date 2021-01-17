dbf9e262c14c43b0aa58c2a277c8be42bc8fdcab
kamil.weclewski <k.t.weclewski@gmail.com>
2021-01-17 22:30:24 +0100

    intro to creation entire questionnaire with data binding

--- Frontend/src/components/QuestionsAddingArea.vue
+++ Frontend/src/components/QuestionsAddingArea.vue
@@ -1,37 +1,57 @@
 <template>
   <div class="QuestionAddingArea">
-    <p>#</p>
-    <p>slider</p>
-    <question-slider/>
-    <p>rating</p>
-    <question-rating/>
-    <p>scale</p>
-    <question-scale/>
-    <p>choice single</p>
-    <question-choice-single/>
-    <p>choice multiple</p>
-    <question-choice-multiple/>
+    <b-button v-on:click="showQuestions" variant="primary">Dodaj nowe pytanie</b-button>
+    <div v-if="this.questionVisible==true">
+      <div class="questionsDivLine">
+        <div v-on:click="addQuestionInParent('singleChoice')" class="questionDivTuple">Pojedynczy wybór</div>
+        <div v-on:click="addQuestionInParent('multipleChoice')" class="questionDivTuple">Wielokrotny wybór</div>
+      </div>
+      <div class="questionsDivLine">
+        <div v-on:click="addQuestionInParent('scale')" class="questionDivTuple">Skala</div>
+        <div v-on:click="addQuestionInParent('slider')" class="questionDivTuple">Suwak</div>
+        <div v-on:click="addQuestionInParent('rating')" class="questionDivTuple">Ocena</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import QuestionScale from "@/components/QuestionScale";
-import QuestionChoiceSingle from "@/components/QuestionChoiceSingle";
-import QuestionChoiceMultiple from "@/components/QuestionChoiceMultiple";
-import QuestionRating from "@/components/QuestionRating";
-import QuestionSlider from "@/components/QuestionSlider";
 export default {
   name: "QuestionAddingArea",
+  data(){
+    return{
+      questionVisible: false
+    }
+  },
   components: {
-    QuestionSlider,
-    QuestionChoiceMultiple,
-    QuestionChoiceSingle,
-    QuestionScale,
-    QuestionRating
+  },
+  methods: {
+    showQuestions: function (){
+      try{
+        this.questionVisible = !this.questionVisible;
+      }catch (msg){
+        console.log(msg)
+      }
+    },
+    addQuestionInParent: function (typeOfQuestion){
+      this.$emit('newQuestionToAdd', typeOfQuestion)
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.questionsDivLine{
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+}
+.questionDivTuple{
+  flex: 1;
+  padding: 0.2em;
+  border: 0.1em outset #ccc;
+  border-radius: 0.5em;
+  margin: 0.2em;
+  font-size: 1em;
+}
 </style>
--- Frontend/src/views/NewQuestionnaire.vue
+++ Frontend/src/views/NewQuestionnaire.vue
@@ -3,7 +3,10 @@
     <form >
       <input class="form-control" type="text" name="questionaryName" value="" placeholder="Nazwa ankiety..." v-model="Questionary.questionaryName"/>
       <textarea class="form-control" v-model="Questionary.description" placeholder="Opis ankiety"/>
-      <QuestionsAddingArea/>
+<!--      <div v-for="question in Questionary.questions"  :key="answer" :aria-describedby="ariaDescribedby">-->
+
+<!--      </div>-->
+      <QuestionsAddingArea v-on:newQuestionToAdd="newQuestion" />
       <input style="margin: 2%; width:96%" class="form-control" type="date"/>
       <button style="margin-right: 2%" class="btn btn-outline-dark" type="button" >Zapisz</button>
     </form>
@@ -19,8 +22,23 @@ export default {
       Questionary: {
           questionaryName: '',
           description: '',
-          questions: [
-            {}
+          questions: [],
+          questionsByType: [
+            {
+              choices: [
+                {
+                  type: 'single',
+                  questions: []
+                },
+                {
+                  type: 'multiple',
+                  questions: []
+                }
+              ],
+              ratings: [],
+              scales: [],
+              sliders: []
+            }
           ],
           finishDate: ''
         }
@@ -28,9 +46,38 @@ export default {
   },
   components:{
     QuestionsAddingArea
+  },
+  methods:{
+    newQuestion: function (type){
+      try {
+        if (type == "singleChoice"){
+          console.log("singleChoice")
+        }else if (type == "multipleChoice"){
+          console.log("multipleChoice")
+        }else if (type == "scale"){
+          console.log("scale")
+        }else if (type == "slider"){
+          console.log("slider")
+        }else if (type == "rating"){
+          console.log("rating")
+        }else{
+          throw("Bad question type");
+        }
+      }catch (msg){
+        console.log(msg)
+      }
+    }
   }
 };
 </script>
 <style scoped>
-
+input{
+  margin-bottom: 1em;
+}
+textarea{
+  margin-bottom: 1em;
+}
+div{
+  margin-bottom: 1em;
+}
 </style>
